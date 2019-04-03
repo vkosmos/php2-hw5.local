a833a9129d758b4d24d1386d3c5e3e66bfa6f26e
vkosmos <korolev.dm.va@gmail.com>
2019-04-03 15:06:09 +0300

    Исправлены недочеты

--- index.php
+++ index.php
@@ -6,27 +6,25 @@ use App\Errors\Exception404;
 use App\Errors\ExceptionDb;
 use App\Router;
 
-use App\Controllers\Index as III;
-
 require __DIR__ . '/config/lib.php';
 require __DIR__ . '/App/autoload.php';
 
 $controllerName = Router::processRoute( parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) );
 
 try{
-
     if (class_exists($controllerName, true)) {
         $controller = new $controllerName;
         $controller();
     }else{
         throw new Exception404('Не существует контроллера, соответствующего запросу');
     };
-
 }catch(ExceptionDb $e){
+
     \App\Logger::logError($e);
     $erController = new EDb();
     $erController();
 }catch(Exception404 $e){
+
     \App\Logger::logError($e);
     $erController = new E404();
     $erController();
